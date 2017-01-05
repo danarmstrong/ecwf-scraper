a7ac4940cd925d7f6e5eb819f0a1c9761503c1f5
Dan Armstrong <dan.d.armstrong@me.com>
2017-01-05 15:27:16 -0700

    Added empty sqlite database Data is now written to SQLite and JSON file Images are downloaded and thumbnails created Better handling of descriptions

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
-import json
-import re
+import os, random, string, json, re, requests, sqlite3, shutil
 from lxml import html
-import requests
+from PIL import Image
+
+
+def randomword(length):
+    return ''.join(random.choice(string.ascii_letters) for i in range(length))
+
 
 url = 'http://www.easterncoloradowildflowers.com/'
+img_dir = 'img'
+db_template = 'flower_data.sqlite'
+db_file = 'flower_data.db'
+json_file = 'flower_data.json'
+
+if os.path.exists(img_dir):
+    os.rmdir(img_dir)
+os.mkdir(img_dir)
+
+if os.path.exists(db_file):
+    os.remove(db_file)
+shutil.copyfile(db_template, db_file)
+conn = sqlite3.connect(db_file)
+cursor = conn.cursor()
 
 link_page = requests.get(url + '_Scientific.htm')
 link_tree = html.fromstring(link_page.content)
@@ -20,12 +38,21 @@ for link in links:
     common_name = ''
     full_family = ''
     common_family = ''
-    flower_obj = {}
+    flower_obj = {
+        'scientificName': '',
+        'commonName': '',
+        'scientificFamily': '',
+        'commonFamily': '',
+        'description': '',
+        'zone': '',
+        'origin': '',
+        'bloom': '',
+        'thumbnail': ''
+    }
 
-    flower_obj['images'] = []
-    for e in flower_tree.xpath('//img'):
-        flower_obj['images'].append(e.get('src'))
-
+    #################################
+    ## Name
+    #################################
     for e in flower_tree.xpath('//body/table[1]/tr[1]/td[1]'):
         full_name = ' '.join(e.text_content().replace('\n', '').replace('\r', '').split())
 
@@ -39,6 +66,9 @@ for link in links:
     flower_obj['commonName'] = common_name
     flower_obj['scientificName'] = full_name.replace('(' + common_name + ')', '').rstrip()
 
+    #################################
+    ## Family
+    #################################
     for e in flower_tree.xpath('//body/table[1]/tr[1]/td[3]'):
         full_family = ' '.join(e.text_content().replace('\n', '').replace('\r', '').split())
 
@@ -56,24 +86,102 @@ for link in links:
     flower_obj['commonFamily'] = common_family
     flower_obj['scientificFamily'] = full_family.replace('(' + common_family + ')', '').rstrip()
 
+    #################################
+    ## Description
+    #################################
     for e in flower_tree.xpath('//body/font[1]'):
         flower_obj['description'] = ' '.join(e.text_content().replace('\n', '').replace('\r', '').split())
 
+    if len(flower_obj['description']) is 0:
+        for e in flower_tree.xpath('//body/font[2]'):
+            flower_obj['description'] = ' '.join(e.text_content().replace('\n', '').replace('\r', '').split())
+
+    if len(flower_obj['description']) is 0:
+        print('Page `' + link.get('href') + '` has no description')
+
+    #################################
+    ## Zone
+    #################################
     for e in flower_tree.xpath('//body/table[2]/tr[1]/td[2]'):
         flower_obj['zone'] = ' '.join(e.text_content().replace('\n', '').replace('\r', '').split())
 
+    #################################
+    ## Bloom
+    #################################
     for e in flower_tree.xpath('//body/table[2]/tr[2]/td[2]'):
         flower_obj['bloom'] = ' '.join(e.text_content().replace('\n', '').replace('\r', '').split())
 
+    #################################
+    ## Origin
+    #################################
     for e in flower_tree.xpath('//body/table[2]/tr[3]/td[2]'):
         flower_obj['origin'] = ' '.join(e.text_content().replace('\n', '').replace('\r', '').split())
+
+    #################################
+    ## Images
+    #################################
+    flower_obj['images'] = []
+    create_thumb = True
+    for e in flower_tree.xpath('//img'):
+        r = requests.get(url + e.get('src'), stream=True)
+        if r.status_code == 200:
+            filename = randomword(10) + '.jpg'
+            with open(img_dir + '/' + filename, 'wb') as f:
+                for chunk in r.iter_content(1024):
+                    f.write(chunk)
+                f.close()
+            flower_obj['images'].append(filename)
+            if create_thumb:
+                thumb_filename = 'thumb_' + randomword(10) + '.jpg'
+                im = Image.open(img_dir + '/' + filename)
+                im.thumbnail((150, 150), Image.ANTIALIAS)
+                im.save(img_dir + '/' + thumb_filename)
+                flower_obj['thumbnail'] = thumb_filename
+                create_thumb = False
+
+    ##########################################
+    ## Write the data to the SQLite database
+    ##########################################
+    cursor.execute("""
+      INSERT INTO flowers(
+        scientificName,
+        commonName,
+        scientificFamily,
+        commonFamily,
+        description,
+        zone,
+        bloom,
+        origin,
+        thumbnail
+      ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+    """, (
+        flower_obj['scientificName'],
+        flower_obj['commonName'],
+        flower_obj['scientificFamily'],
+        flower_obj['commonFamily'],
+        flower_obj['description'],
+        flower_obj['zone'],
+        flower_obj['bloom'],
+        flower_obj['origin'],
+        flower_obj['thumbnail']
+    ))
+    conn.commit()
+
+    last_id = cursor.lastrowid
+
+    for i in flower_obj['images']:
+        cursor.execute("""
+          INSERT INTO images(flowerId, filename) VALUES(?, ?)
+        """, (last_id, i))
+    conn.commit()
 
     flower_list.append(flower_obj)
 
 flower_json = json.dumps(flower_list, indent=4, sort_keys=True)
 
-f = open('flower_data.json', 'w')
+f = open(json_file, 'w')
 f.write(flower_json)
 f.close()
 
+conn.close()
 print('Done!')
